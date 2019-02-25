c5e35b4c33ce7a3a3c2c89202bf1d9316d5a3438
Thomas Bennett <tombennett86@hotmail.co.uk>
2019-02-25 21:52:08 +0000

    do do mongo client

--- repository/mongo_article.go
+++ repository/mongo_article.go
@@ -24,72 +24,128 @@ type MongoArticleRepository struct {
 	db           *mongo.Database
 }
 
-type key string
+// type key string
 
-const (
-	hostKey         = key("hostKey")
-	usernameKey     = key("usernameKey")
-	passwordKey     = key("passwordKey")
-	databaseKey     = key("databaseKey")
-	authDatabaseKey = key("authDatabaseKey")
-	dBSSL           = key("dBSSL")
-)
+// const (
+// 	hostKey         = key("hostKey")
+// 	usernameKey     = key("usernameKey")
+// 	passwordKey     = key("passwordKey")
+// 	databaseKey     = key("databaseKey")
+// 	authDatabaseKey = key("authDatabaseKey")
+// 	dBSSL           = key("dBSSL")
+// )
 
 // Connect to the db instance
-func (r *MongoArticleRepository) Connect() {
-	log.Print("Connecting...")
-	ctx := context.Background()
-	ctx, cancel := context.WithCancel(ctx)
-	defer cancel()
-	ctx = context.WithValue(ctx, hostKey, r.Server)
-	ctx = context.WithValue(ctx, usernameKey, r.Username)
-	ctx = context.WithValue(ctx, passwordKey, r.Password)
-	ctx = context.WithValue(ctx, databaseKey, r.DatabaseName)
-	ctx = context.WithValue(ctx, authDatabaseKey, r.AuthDatabase)
-	ctx = context.WithValue(ctx, dBSSL, r.DBSSL)
-	db, err := configDB(ctx)
-	if err != nil {
-		log.Fatalf("todo: database configuration failed: %v", err)
-	}
-	r.db = db
-}
+// func (r *MongoArticleRepository) Connect() {
+// 	log.Print("Connecting...")
+// 	ctx := context.Background()
+// 	ctx, cancel := context.WithCancel(ctx)
+// 	defer cancel()
+// 	ctx = context.WithValue(ctx, hostKey, r.Server)
+// 	ctx = context.WithValue(ctx, usernameKey, r.Username)
+// 	ctx = context.WithValue(ctx, passwordKey, r.Password)
+// 	ctx = context.WithValue(ctx, databaseKey, r.DatabaseName)
+// 	ctx = context.WithValue(ctx, authDatabaseKey, r.AuthDatabase)
+// 	ctx = context.WithValue(ctx, dBSSL, r.DBSSL)
+// 	db, err := configDB(ctx)
+// 	if err != nil {
+// 		log.Fatalf("todo: database configuration failed: %v", err)
+// 	}
+// 	r.db = db
+// }
 
-func configDB(ctx context.Context) (*mongo.Database, error) {
+// func configDB(ctx context.Context) (*mongo.Database, error) {
+// 	uri := fmt.Sprintf(`mongodb://%s:%s@%s/%s?authSource=%s&ssl=%s`,
+// 		ctx.Value(usernameKey),
+// 		ctx.Value(passwordKey),
+// 		ctx.Value(hostKey),
+// 		ctx.Value(databaseKey),
+// 		ctx.Value(authDatabaseKey),
+// 		ctx.Value(dBSSL),
+// 	)
+// 	log.Print(uri)
+// 	client, err := mongo.NewClient(uri)
+// 	if err != nil {
+// 		return nil, fmt.Errorf("todo: couldn't connect to mongo: %v", err)
+// 	}
+// 	err = client.Connect(ctx)
+// 	if err != nil {
+// 		return nil, fmt.Errorf("todo: mongo client couldn't connect with background context: %v", err)
+// 	}
+// 	dbName := ctx.Value(databaseKey).(string)
+// 	articleDB := client.Database(dbName)
+// 	return articleDB, nil
+// }
+
+// Insert a record to the db
+// func (r *MongoArticleRepository) Insert(a model.Article) (model.Article, error) {
+// 	log.Printf("Attempting insert: %s", a.URL)
+// 	r.Connect()
+// 	collection := r.db.Collection(r.Collection)
+// 	cur, err := collection.CountDocuments(context.Background(), bson.M{"url": a.URL})
+// 	if err != nil {
+// 		return a, err
+// 	}
+// 	if cur > 0 {
+// 		return a, fmt.Errorf("resource %s already exists", a.URL)
+// 	}
+// 	a.CreatedAt = time.Now()
+
+// 	res, err := collection.InsertOne(context.Background(), bson.M{
+// 		"title":      a.Title,
+// 		"url":        a.URL,
+// 		"published":  a.Published,
+// 		"created_at": a.CreatedAt,
+// 	})
+
+// 	if err != nil {
+// 		return a, err
+// 	}
+
+// 	log.Printf("Inserted record: %s", res.InsertedID)
+// 	return a, nil
+// }
+
+// Connect to the db instance
+func (r *MongoArticleRepository) Connect() (*mongo.Client, error) {
+	log.Print("Connecting...")
+
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+
 	uri := fmt.Sprintf(`mongodb://%s:%s@%s/%s?authSource=%s&ssl=%s`,
-		ctx.Value(usernameKey),
-		ctx.Value(passwordKey),
-		ctx.Value(hostKey),
-		ctx.Value(databaseKey),
-		ctx.Value(authDatabaseKey),
-		ctx.Value(dBSSL),
+		r.Username,
+		r.Password,
+		r.Server,
+		r.DatabaseName,
+		r.AuthDatabase,
+		r.DBSSL,
 	)
+
 	log.Print(uri)
-	client, err := mongo.NewClient(uri)
-	if err != nil {
-		return nil, fmt.Errorf("todo: couldn't connect to mongo: %v", err)
-	}
-	err = client.Connect(ctx)
-	if err != nil {
-		return nil, fmt.Errorf("todo: mongo client couldn't connect with background context: %v", err)
-	}
-	dbName := ctx.Value(databaseKey).(string)
-	articleDB := client.Database(dbName)
-	defer client.Disconnect(ctx)
-	return articleDB, nil
+
+	client, err := mongo.Connect(ctx, uri)
+
+	return client, err
 }
 
 // Insert a record to the db
 func (r *MongoArticleRepository) Insert(a model.Article) (model.Article, error) {
-	log.Printf("Attempting insert: %s", a.URL)
-	r.Connect()
-	collection := r.db.Collection(r.Collection)
-	cur, err := collection.CountDocuments(context.Background(), bson.M{"url": a.URL})
+	client, err := r.Connect()
+
+	if err != nil {
+		log.Fatalf("todo: database configuration failed: %v", err)
+	}
+
+	collection := client.Database(r.DatabaseName).Collection(r.Collection)
+	matches, err := collection.CountDocuments(context.Background(), bson.M{"url": a.URL})
 	if err != nil {
 		return a, err
 	}
-	if cur > 0 {
+	if matches > 0 {
 		return a, fmt.Errorf("resource %s already exists", a.URL)
 	}
+
 	a.CreatedAt = time.Now()
 
 	res, err := collection.InsertOne(context.Background(), bson.M{
@@ -98,6 +154,12 @@ func (r *MongoArticleRepository) Insert(a model.Article) (model.Article, error) 
 		"published":  a.Published,
 		"created_at": a.CreatedAt,
 	})
+
+	if err != nil {
+		return a, err
+	}
+
+	err = client.Disconnect(context.TODO())
 
 	if err != nil {
 		return a, err
