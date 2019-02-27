a4b9fb51a871a6530a6821e5f979c81ebb700ac0
Thomas Bennett <tombennett86@hotmail.co.uk>
2019-02-27 21:00:37 +0000

    Add commends to deployment script

--- deployment/deploy.sh
+++ deployment/deploy.sh
@@ -12,6 +12,7 @@ if [[ $((helm) 2>&1 | grep "command not found" ) ]]; then
     curl https://raw.githubusercontent.com/helm/helm/master/scripts/get > get_helm.sh
     chmod 700 get_helm.sh
     ./get_helm.sh
+    echo "creating service tiller account"
     kubectl --namespace kube-system create sa tiller
     kubectl create clusterrolebinding tiller \
     --clusterrole cluster-admin \
