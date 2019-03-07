97666f5945bba3b52a958d6d89b2a968b1940ee3
Bennett, Thomas <thomas.bennett@sky.uk>
2019-03-07 12:26:54 +0000

    Add tiller service account creation

--- terraform/deploy/main.tf
+++ terraform/deploy/main.tf
@@ -14,6 +14,32 @@ provider "kubernetes" {
   cluster_ca_certificate = "${var.cluster_ca_certificate}"
 }
 
+resource "kubernetes_service_account" "tiller" {
+  metadata {
+    name      = "tiller"
+    namespace = "kube-system"
+  }
+}
+
+resource "kubernetes_cluster_role_binding" "tiller" {
+  metadata {
+    name = "tiller"
+  }
+
+  role_ref {
+    api_group = "rbac.authorization.k8s.io"
+    kind      = "ClusterRole"
+    name      = "cluster-admin"
+  }
+
+  subject {
+    api_group = ""
+    kind      = "ServiceAccount"
+    name      = "tiller"
+    namespace = "kube-system"
+  }
+}
+
 resource "helm_release" "article-ingest-k8s" {
   name      = "article-ingest-k8s"
   chart     = "../../article-ingest-k8s"
