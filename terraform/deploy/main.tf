5adbf2fd0b044d1424f0044020fc337a03a09d14
Bennett, Thomas <thomas.bennett@sky.uk>
2019-03-11 15:19:17 +0000

    Remove duplicate k8s stuff

--- terraform/deploy/main.tf
+++ terraform/deploy/main.tf
@@ -1,51 +1,47 @@
-provider "helm" {
-  version = "~> 0.6"
-  service_account = "${kubernetes_service_account.tiller.metadata.0.name}"
+# provider "helm" {
+#   version = "~> 0.6"
+#   service_account = "${kubernetes_service_account.tiller.metadata.0.name}"
 
-  kubernetes {
-    host                   = "${var.host}"
-    token                  = "${var.token}"
-    #client_certificate     = "${var.client_certificate}"
-    #client_key             = "${var.client_key}"
-    cluster_ca_certificate = "${var.cluster_ca_certificate}"
-  }
-}
+#   kubernetes {
+#     host                   = "${var.host}"
+#     token                  = "${var.token}"
+#     cluster_ca_certificate = "${var.cluster_ca_certificate}"
+#   }
+# }
 
-provider "kubernetes" {
-  host                   = "${var.host}"
-  token                  = "${var.token}"
-  #client_certificate     = "${var.client_certificate}"
-  #client_key             = "${var.client_key}"
-  cluster_ca_certificate = "${var.cluster_ca_certificate}"
+# provider "kubernetes" {
+#   host                   = "${var.host}"
+#   token                  = "${var.token}"
+#   cluster_ca_certificate = "${var.cluster_ca_certificate}"
 
-  load_config_file = false
-}
+#   load_config_file = false
+# }
 
-resource "kubernetes_service_account" "tiller" {
-  metadata {
-    name      = "tiller"
-    namespace = "kube-system"
-  }
-}
+# resource "kubernetes_service_account" "tiller" {
+#   metadata {
+#     name      = "tiller"
+#     namespace = "kube-system"
+#   }
+# }
 
-resource "kubernetes_cluster_role_binding" "tiller" {
-  metadata {
-    name = "tiller"
-  }
+# resource "kubernetes_cluster_role_binding" "tiller" {
+#   metadata {
+#     name = "tiller"
+#   }
 
-  role_ref {
-    api_group = "rbac.authorization.k8s.io"
-    kind      = "ClusterRole"
-    name      = "cluster-admin"
-  }
+#   role_ref {
+#     api_group = "rbac.authorization.k8s.io"
+#     kind      = "ClusterRole"
+#     name      = "cluster-admin"
+#   }
 
-  subject {
-    api_group = ""
-    kind      = "ServiceAccount"
-    name      = "tiller"
-    namespace = "kube-system"
-  }
-}
+#   subject {
+#     api_group = ""
+#     kind      = "ServiceAccount"
+#     name      = "tiller"
+#     namespace = "kube-system"
+#   }
+# }
 
 resource "helm_release" "article-ingest-k8s" {
     depends_on = ["kubernetes_service_account.tiller"]
