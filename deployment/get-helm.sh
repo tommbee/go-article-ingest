fc57d6506bb4ff7b3222da678befc5bb2540af21
Bennett, Thomas <thomas.bennett@sky.uk>
2019-03-12 22:26:51 +0000

    install helm on CI client

---
+++ deployment/get-helm.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+## install helm
+echo "Check Helm is installed..."
+if [[ $((helm) 2>&1 | grep "command not found" ) ]]; then
+    echo "Installing Helm"
+    curl https://raw.githubusercontent.com/helm/helm/master/scripts/get > get_helm.sh
+    chmod 700 get_helm.sh
+    ./get_helm.sh
+fi
