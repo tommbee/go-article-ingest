203a9f8983766d4add943e5e220ba53829de599e
Bennett, Thomas <thomas.bennett@sky.uk>
2019-03-12 06:50:29 +0000

    Comment out app deployment

--- terraform/main.tf
+++ terraform/main.tf
@@ -11,27 +11,27 @@ module "article-app" {
   config_file = "${var.config_file}"
 }
 
-module "deploy" {
-    source = "./deploy"
+# module "deploy" {
+#     source = "./deploy"
 
-    # client_certificate = "${base64decode(module.article-app.client_certificate)}"
-    # client_key = "${base64decode(module.article-app.client_key)}"
-    # cluster_ca_certificate = "${base64decode(module.article-app.cluster_ca_certificate)}"
-    # host = "${module.article-app.host}"
-    # token = "${module.article-app.token}"
-    # service_account_name = "${module.article-app.helm_service_account}"
+#     # client_certificate = "${base64decode(module.article-app.client_certificate)}"
+#     # client_key = "${base64decode(module.article-app.client_key)}"
+#     # cluster_ca_certificate = "${base64decode(module.article-app.cluster_ca_certificate)}"
+#     # host = "${module.article-app.host}"
+#     # token = "${module.article-app.token}"
+#     # service_account_name = "${module.article-app.helm_service_account}"
 
-    ## app specific
-    image_repository = "${var.image_repository}"
-    image_tag = "${var.image_tag}"
-    sources = "${var.sources}"
-    server = "${var.server}"
-    db = "${var.db}"
-    config_file_location = "${var.config_file_location}"
-    article_collection = "${var.article_collection}"
-    db_user = "${var.db_user}"
-    db_password = "${var.db_password}"
-    auth_db = "${var.auth_db}"
-    db_ssl = "${var.db_ssl}"
-    namespace = "${var.namespace}"
-}
+#     ## app specific
+#     image_repository = "${var.image_repository}"
+#     image_tag = "${var.image_tag}"
+#     sources = "${var.sources}"
+#     server = "${var.server}"
+#     db = "${var.db}"
+#     config_file_location = "${var.config_file_location}"
+#     article_collection = "${var.article_collection}"
+#     db_user = "${var.db_user}"
+#     db_password = "${var.db_password}"
+#     auth_db = "${var.auth_db}"
+#     db_ssl = "${var.db_ssl}"
+#     namespace = "${var.namespace}"
+# }
