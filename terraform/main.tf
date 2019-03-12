501d6e99752e32d09c770a6916064e26c19dfb03
Bennett, Thomas <thomas.bennett@sky.uk>
2019-03-12 19:17:16 +0000

    test

--- terraform/main.tf
+++ terraform/main.tf
@@ -76,114 +76,114 @@ module "article-app-cluster" {
 #   }
 # }
 
-provider "kubernetes" {
-  #client_certificate = "${module.article-app-cluster.client_certificate}"
-  #client_key = "${module.article-app-cluster.client_key}"
-  cluster_ca_certificate = "${module.article-app-cluster.cluster_ca_certificate}"
-  host = "${module.article-app-cluster.host}"
-  token = "${module.article-app-cluster.token}"
+# provider "kubernetes" {
+#   #client_certificate = "${module.article-app-cluster.client_certificate}"
+#   #client_key = "${module.article-app-cluster.client_key}"
+#   cluster_ca_certificate = "${module.article-app-cluster.cluster_ca_certificate}"
+#   host = "${module.article-app-cluster.host}"
+#   token = "${module.article-app-cluster.token}"
 
-  load_config_file = false
-}
+#   load_config_file = false
+# }
 
-resource "kubernetes_service_account" "tiller" {
-  metadata {
-    name      = "tiller"
-    namespace = "kube-system"
-  }
+# resource "kubernetes_service_account" "tiller" {
+#   metadata {
+#     name      = "tiller"
+#     namespace = "kube-system"
+#   }
   
-  automount_service_account_token = true
-}
+#   automount_service_account_token = true
+# }
 
-resource "kubernetes_cluster_role_binding" "tiller" {
-  metadata {
-    name = "tiller"
-  }
+# resource "kubernetes_cluster_role_binding" "tiller" {
+#   metadata {
+#     name = "tiller"
+#   }
 
-  role_ref {
-    api_group = "rbac.authorization.k8s.io"
-    kind      = "ClusterRole"
-    name      = "cluster-admin"
-  }
+#   role_ref {
+#     api_group = "rbac.authorization.k8s.io"
+#     kind      = "ClusterRole"
+#     name      = "cluster-admin"
+#   }
 
-  subject {
-    api_group = ""
-    kind      = "ServiceAccount"
-    name      = "tiller"
-    namespace = "kube-system"
-  }
-}
+#   subject {
+#     api_group = ""
+#     kind      = "ServiceAccount"
+#     name      = "tiller"
+#     namespace = "kube-system"
+#   }
+# }
 
-variable "helm_version" {
-  default = "v2.9.1"
-}
+# variable "helm_version" {
+#   default = "v2.9.1"
+# }
 
-provider "helm" {
-  service_account = "${kubernetes_service_account.tiller.metadata.0.name}"
-  namespace       = "${kubernetes_service_account.tiller.metadata.0.namespace}"
-  #install_tiller  = false
+# provider "helm" {
+#   service_account = "${kubernetes_service_account.tiller.metadata.0.name}"
+#   namespace       = "${kubernetes_service_account.tiller.metadata.0.namespace}"
+#   #install_tiller  = false
 
-  kubernetes {
-    #client_certificate     = "${var.client_certificate}"
-    #client_key             = "${var.client_key}"
-    cluster_ca_certificate = "${module.article-app-cluster.cluster_ca_certificate}"
-    host = "${module.article-app-cluster.host}"
-    token = "${module.article-app-cluster.token}"
-  }
-}
+#   kubernetes {
+#     #client_certificate     = "${var.client_certificate}"
+#     #client_key             = "${var.client_key}"
+#     cluster_ca_certificate = "${module.article-app-cluster.cluster_ca_certificate}"
+#     host = "${module.article-app-cluster.host}"
+#     token = "${module.article-app-cluster.token}"
+#   }
+# }
 
-resource "helm_release" "article-ingest-k8s" {
-    #depends_on = ["kubernetes_service_account.tiller"]
-    name      = "article-ingest-k8s"
-    chart     = "../article-ingest-k8s"
-    namespace = "${var.namespace}"
-    depends_on = [
-      "kubernetes_service_account.tiller",
-      "kubernetes_cluster_role_binding.tiller",
-    ]
+# resource "helm_release" "article-ingest-k8s" {
+#     #depends_on = ["kubernetes_service_account.tiller"]
+#     name      = "article-ingest-k8s"
+#     chart     = "../article-ingest-k8s"
+#     namespace = "${var.namespace}"
+#     depends_on = [
+#       "kubernetes_service_account.tiller",
+#       "kubernetes_cluster_role_binding.tiller",
+#     ]
 
-    set {
-        name  = "image.repository"
-        value = "${var.image_repository}"
-    }
-    set {
-        name  = "image.tag"
-        value = "${var.image_tag}"
-    }
-    set {
-        name  = "sources"
-        value = "${var.sources}"
-    }
-    set {
-        name  = "server"
-        value = "${var.server}"
-    }
-    set {
-        name  = "db"
-        value = "${var.db}"
-    }
-    set {
-        name  = "configFileLocation"
-        value = "${var.config_file_location}"
-    }
-    set {
-        name  = "articleCollection"
-        value = "${var.article_collection}"
-    }
-    set {
-        name  = "dbUser"
-        value = "${var.db_user}"
-    }
-    set {
-        name  = "dbPassword"
-        value = "${var.db_password}"
-    }
-    set {
-        name  = "authDb"
-        value = "${var.auth_db}"
-    }
-    set {
-        name  = "dbSsl"
-        value = "${var.db_ssl}"
-    }
-}
+#     set {
+#         name  = "image.repository"
+#         value = "${var.image_repository}"
+#     }
+#     set {
+#         name  = "image.tag"
+#         value = "${var.image_tag}"
+#     }
+#     set {
+#         name  = "sources"
+#         value = "${var.sources}"
+#     }
+#     set {
+#         name  = "server"
+#         value = "${var.server}"
+#     }
+#     set {
+#         name  = "db"
+#         value = "${var.db}"
+#     }
+#     set {
+#         name  = "configFileLocation"
+#         value = "${var.config_file_location}"
+#     }
+#     set {
+#         name  = "articleCollection"
+#         value = "${var.article_collection}"
+#     }
+#     set {
+#         name  = "dbUser"
+#         value = "${var.db_user}"
+#     }
+#     set {
+#         name  = "dbPassword"
+#         value = "${var.db_password}"
+#     }
+#     set {
+#         name  = "authDb"
+#         value = "${var.auth_db}"
+#     }
+#     set {
+#         name  = "dbSsl"
+#         value = "${var.db_ssl}"
+#     }
+# }
