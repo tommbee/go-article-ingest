4c03ef4f831dfe33a6ad700d57ef5c5ce6e5b698
Bennett, Thomas <thomas.bennett@sky.uk>
2019-03-12 23:25:56 +0000

    add helm repo

--- deployment/get-helm.sh
+++ deployment/get-helm.sh
@@ -10,4 +10,5 @@ if [[ $((helm) 2>&1 | grep "command not found" ) ]]; then
     chmod 700 get_helm.sh
     ./get_helm.sh
     helm init --client-only
+    helm repo add coreos https://s3-eu-west-1.amazonaws.com/coreos-charts/stable/
 fi
