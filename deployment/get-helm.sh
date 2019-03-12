2b6bfd0a4664f4f62a3b6de077208b5b58b90249
Bennett, Thomas <thomas.bennett@sky.uk>
2019-03-12 23:06:17 +0000

    test

--- deployment/get-helm.sh
+++ deployment/get-helm.sh
@@ -7,4 +7,5 @@ if [[ $((helm) 2>&1 | grep "command not found" ) ]]; then
     curl https://raw.githubusercontent.com/helm/helm/master/scripts/get > get_helm.sh
     chmod 700 get_helm.sh
     ./get_helm.sh
+    helm init --client-only
 fi
