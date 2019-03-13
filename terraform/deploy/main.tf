2c7078154f79a040ef39bb3f01c29d7ecc36b5a1
Bennett, Thomas <thomas.bennett@sky.uk>
2019-03-13 21:47:57 +0000

    dont deploy app

--- terraform/deploy/main.tf
+++ terraform/deploy/main.tf
@@ -1,81 +1,81 @@
-provider "helm" {
-  version = "~> 0.8.0"
-  service_account = "${var.helm_service_account}"
-  namespace       = "${var.helm_namespace}"
-  install_tiller  = true
-  tiller_image    = "gcr.io/kubernetes-helm/tiller:v2.12.1"
+# provider "helm" {
+#   version = "~> 0.8.0"
+#   service_account = "${var.helm_service_account}"
+#   namespace       = "${var.helm_namespace}"
+#   install_tiller  = true
+#   tiller_image    = "gcr.io/kubernetes-helm/tiller:v2.12.1"
 
-  kubernetes {
-    #client_certificate     = "${var.client_certificate}"
-    #client_key             = "${var.client_key}"
-    cluster_ca_certificate = "${var.cluster_ca_certificate}"
-    host                   = "${var.host}"
-    token                  = "${var.token}"
-  }
-}
+#   kubernetes {
+#     #client_certificate     = "${var.client_certificate}"
+#     #client_key             = "${var.client_key}"
+#     cluster_ca_certificate = "${var.cluster_ca_certificate}"
+#     host                   = "${var.host}"
+#     token                  = "${var.token}"
+#   }
+# }
 
-resource "null_resource" "depends_on_hack" {
-  triggers {
-    version = "${timestamp()}"
-  }
+# resource "null_resource" "depends_on_hack" {
+#   triggers {
+#     version = "${timestamp()}"
+#   }
 
-  connection {
-    service_account = "${var.helm_service_account}"
-    namespace       = "${var.helm_namespace}"
-  }
-}
+#   connection {
+#     service_account = "${var.helm_service_account}"
+#     namespace       = "${var.helm_namespace}"
+#   }
+# }
 
-resource "helm_release" "article-ingest-k8s" {
-    name      = "article-ingest-k8s"
-    chart     = "../article-ingest-k8s"
-    namespace = "${var.namespace}"
+# resource "helm_release" "article-ingest-k8s" {
+#     name      = "article-ingest-k8s"
+#     chart     = "../article-ingest-k8s"
+#     namespace = "${var.namespace}"
 
-    depends_on = [
-        "null_resource.depends_on_hack",
-    ]
+#     depends_on = [
+#         "null_resource.depends_on_hack",
+#     ]
 
-    set {
-        name  = "image.repository"
-        value = "${var.image_repository}"
-    }
-    set {
-        name  = "image.tag"
-        value = "${var.image_tag}"
-    }
-    set {
-        name  = "sources"
-        value = "${var.sources}"
-    }
-    set {
-        name  = "server"
-        value = "${var.server}"
-    }
-    set {
-        name  = "db"
-        value = "${var.db}"
-    }
-    set {
-        name  = "configFileLocation"
-        value = "${var.config_file_location}"
-    }
-    set {
-        name  = "articleCollection"
-        value = "${var.article_collection}"
-    }
-    set {
-        name  = "dbUser"
-        value = "${var.db_user}"
-    }
-    set {
-        name  = "dbPassword"
-        value = "${var.db_password}"
-    }
-    set {
-        name  = "authDb"
-        value = "${var.auth_db}"
-    }
-    set {
-        name  = "dbSsl"
-        value = "${var.db_ssl}"
-    }
-}
+#     set {
+#         name  = "image.repository"
+#         value = "${var.image_repository}"
+#     }
+#     set {
+#         name  = "image.tag"
+#         value = "${var.image_tag}"
+#     }
+#     set {
+#         name  = "sources"
+#         value = "${var.sources}"
+#     }
+#     set {
+#         name  = "server"
+#         value = "${var.server}"
+#     }
+#     set {
+#         name  = "db"
+#         value = "${var.db}"
+#     }
+#     set {
+#         name  = "configFileLocation"
+#         value = "${var.config_file_location}"
+#     }
+#     set {
+#         name  = "articleCollection"
+#         value = "${var.article_collection}"
+#     }
+#     set {
+#         name  = "dbUser"
+#         value = "${var.db_user}"
+#     }
+#     set {
+#         name  = "dbPassword"
+#         value = "${var.db_password}"
+#     }
+#     set {
+#         name  = "authDb"
+#         value = "${var.auth_db}"
+#     }
+#     set {
+#         name  = "dbSsl"
+#         value = "${var.db_ssl}"
+#     }
+# }
