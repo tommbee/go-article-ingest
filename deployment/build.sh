aaa630a961abd40bf16482437ed215f2ce49c73f
Bennett, Thomas <thomas.bennett@sky.uk>
2019-05-10 22:47:50 +0100

    tweak

--- deployment/build.sh
+++ deployment/build.sh
@@ -6,10 +6,10 @@ echo 'Getting config...'
 echo "Authenticating with GKE..."
 apt-get install -qq -y gettext
 echo $GCLOUD_SERVICE_KEY > ${HOME}/gcloud-service-key.json
-echo $GCLOUD_SERVICE_KEY > ./site-config/auth.json
 gcloud auth activate-service-account --key-file=${HOME}/gcloud-service-key.json
 
 echo "Saving config to cache..."
 mkdir -p site-config
+echo $GCLOUD_SERVICE_KEY > ./site-config/auth.json
 gsutil cp gs://article-app-storage/${CONFIG_FILENAME} ./site-config/${CONFIG_FILENAME}
 gsutil cp gs://article-app-storage/kubeconfig ./site-config/kubeconfig
