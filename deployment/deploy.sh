8ed5527fabe1d43834fdef1bb7d6c370bdd3913d
Bennett, Thomas <thomas.bennett@sky.uk>
2019-05-10 09:18:42 +0100

    Tweak deploy script

--- deployment/deploy.sh
+++ deployment/deploy.sh
@@ -7,7 +7,7 @@ if [[ $((helm) 2>&1 | grep "command not found" ) ]]; then
     curl https://raw.githubusercontent.com/helm/helm/master/scripts/get > get_helm.sh
     chmod 700 get_helm.sh
     ./get_helm.sh
-    helm init --service-account tiller --wait --kubeconfig ../kubeconfig
+    helm init --service-account tiller --wait --kubeconfig ./kubeconfig
     helm repo add coreos https://s3-eu-west-1.amazonaws.com/coreos-charts/stable/
 fi
 
